c8a92c2e31652e1962472c20983856e137505cea
Ravishankar <ravioshankar@outlook.com>
2018-10-10 23:12:57 -0500

    Initial commit with open layer basic map setup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MapComponent } from './modules/map/component/map.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MapComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/modules/map/component/map.component.ts
@@ -0,0 +1,36 @@
+import { Component, OnInit } from '@angular/core';
+import Map from 'ol/Map';
+import Tile from 'ol/layer/Tile';
+import OSM from 'ol/source/OSM';
+import View from 'ol/View';
+
+@Component({
+  selector: 'app-map',
+  templateUrl: './map.component.html',
+  styleUrls: ['./map.component.scss']
+})
+export class MapComponent implements OnInit {
+
+  map;
+  constructor() { }
+
+  ngOnInit() {
+    this.initilizeMap();
+  }
+
+  initilizeMap() {
+
+    this.map = new Map({
+      target: 'map',
+      layers: [
+        new Tile({
+          source: new OSM()
+        })
+      ],
+      view: new View({
+        center: [37.41, 8.82],
+        zoom: 4
+      })
+    });
+  }
+}
