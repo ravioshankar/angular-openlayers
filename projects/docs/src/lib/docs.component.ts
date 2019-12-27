b2d95684c1700d441227302f7f4c2e7da1031d99
Ravi Shankar <ravioshankar@outlook.com>
2019-12-27 14:32:09 -0600

    Added lerna

--- projects/docs/src/lib/docs.component.ts
+++ projects/docs/src/lib/docs.component.ts
@@ -1,17 +1,18 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'lib-docs',
   template: `
-  <iframe id="basic-setup" width="300" height="600"
-  src="https://ravioshankar.github.io/angular-openlayers/index.html">
+  <iframe id="appDocs" width="300" height="600"
+  src="https://ravioshankar.github.io/angular-openlayers/components/AppComponent.html">
 </iframe>
   `,
   styleUrls: ['./docs.component.scss']
 })
 export class DocsComponent implements OnInit {
 
-  constructor() { }
+  constructor() {
+   }
 
   ngOnInit() {
   }
