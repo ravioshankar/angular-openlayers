89dca01115fb9c2c980a202cbe12b51d6bb88e03
Ravi Shankar <ravioshankar@outlook.com>
2022-07-04 14:03:17 -0500

    update material

--- projects/clustered-features/src/lib/clustered-features.module.ts
+++ projects/clustered-features/src/lib/clustered-features.module.ts
@@ -1,7 +1,7 @@
 import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { ClusteredFeaturesComponent } from './clustered-features.component';
 import { YouTubePlayerModule } from '@angular/youtube-player';
-import { MatSliderModule } from '@angular/material';
+import { MatSliderModule } from '@angular/material/slider';
 
 
 
